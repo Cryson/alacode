e89eb2c215dea7269ca97c315daa7bd04df3944a
sasaki <sasaki@MacBook-Pro-M1-Pro.local>
2022-08-30 00:17:54 +0900

    キャッシュファイル削除

--- src/components/Context.tsx
+++
@@ -1,3 +0,0 @@
-import React, { createContext } from 'react';
-
-export const LoadingContext: React.Context<Boolean> = createContext<Boolean>(false);
--- src/style/vw.jsx
+++
@@ -1,6 +0,0 @@
-export const vwChange = (prop, px, viewport) => {
-  return (`${prop}: ${(px / viewport) * 100 }vw;`);
-};
-export const vwRange = (prop, minpx, maxpx, minvw, maxvw) =>  {
-  return (`${prop}: calc(${minpx}px + ${maxpx - minpx} * (100vw - ${minvw}px) / ${maxvw - minvw});` );
-};
